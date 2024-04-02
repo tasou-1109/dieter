1f9d5da9c7908db854015d8688790164d9480ca0
unknown <sakatasinp@gmail.com>
2024-04-02 23:02:39 +0900

    commit

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Set } from "../pages/Set";
 
