f113efacad4cc41146cb43aabd12ad29709115a7
zaki <sakatasinp@gmail.com>
2024-06-04 14:36:48 +0900

    ヘッダー設計

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,3 +1,51 @@
 import "./header.scss";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../supabase";
+import { useNavigate } from "react-router-dom";
 
+export const Header = () => {
+  const auth = supabase.auth.getSession();
+  const Nav = useNavigate();
+  const [userName, setUserName] = useState();
 
+  const handleSignOut = () => {
+    supabase.auth.signOut();
+    Nav("/login");
+  };
+
+  const goLogin = () => {
+    Nav("/login");
+  };
+
+  
+    useEffect(() => {
+        if(auth){
+            setUserName(supabase)
+        }
+    })
+  
+
+  return (
+    <>
+      {auth ? (
+        <header className="header">
+          <h1 className="header__title">ダイエッター</h1>
+          <h3 className="header__name">ユーザ名：{userName}</h3>
+          &emsp;
+          <button onClick={handleSignOut()} className="header__signOut">
+            サインアウト
+          </button>
+        </header>
+      ) : (
+        <header className="header">
+          <h1 className="header__title">ダイエッター</h1>
+          <h3 className="header__name">ユーザ名</h3>
+          &emsp;
+          <button onClick={goLogin()} className="header__signOut">
+            ログイン
+          </button>
+        </header>
+      )}
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Training } from "./Training";
 import { Yotei } from "./Yotei";
 import "./home.scss";
+import { Header } from "../header/Header";
 import { supabase } from "../supabase";
 
 export const Home = () => {
   console.log(supabase.auth.getSession());
   return (
     <div>
+      <Header />
       <main className="main">
         <Yotei />
         <br />
