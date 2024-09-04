1dd9edfa2bb26dffed530a3820a8a12daf087550
zaki <sakatasinp@gmail.com>
2024-09-04 12:24:59 +0900

    セキュリティ修正

--- src/supabase.js
+++ src/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
-
-const supabaseUrl = "https://aelamqkxybedexzigjni.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbGFtcWt4eWJlZGV4emlnam5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTczNzY3MTQsImV4cCI6MjAzMjk1MjcxNH0.jcdc3GwjEobF3bcpsU6hGHcRPqoaKKz5-Mkfo8HA2Lc";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
