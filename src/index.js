d5a5a183e9785a6b3556af2fa175d62cdab96dcc
zaki <sakatasinp@gmail.com>
2024-12-09 11:03:49 +0900

    index 修正

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import AppDieter from "./dieter/AppDieter";
-import { App } from "./culinaryMate/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/culinaryMate/*" element={<App />} />
-        <Route path="/*" element={<AppDieter />} />
-      </Routes>
-    </Router>
+    <AppDieter />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
